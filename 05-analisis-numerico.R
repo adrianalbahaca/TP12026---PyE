df6b81403def4c29411c2ee4dffc3f00d448e07c
Adrian <adrian.albahaca@gmail.com>
2026-04-20 15:39:52 -0300

    Modificaciones al Análisis Numérico

--- 05-analisis-numerico.R
+++ 05-analisis-numerico.R
@@ -1,3 +1,7 @@
+# Instalar paquetes de ser necesario
+# install.packages("tidyselect")
+# install.packages("janitor")
+
 library(tidyselect)
 library(janitor)
 
@@ -6,45 +10,61 @@ str(datos_limpios)
 
 # Algunas medidas resumen 
 summary(datos_limpios)
-summary(datos_limpios[,c(2,3,4)])
+
+# Puedo fijar los datos por comodidad
+attach(datos_limpios)
+
+# Primero, obtengo un top 10 de los países con mayor puntaje en el GIRAI
+top_10 <- datos_limpios %>%
+  dplyr::arrange(desc(GIRAI)) %>%
+  dplyr::select(Pais, GIRAI_region, GIRAI, cap, gob, ddhh) %>%
+  head(10)
+
+# Luego, selecciono a los países con los 10 peores puntajes en el GIRAI
+bottom_10 <- datos_limpios %>%
+  dplyr::arrange(GIRAI) %>%
+  dplyr::select(Pais, GIRAI_region, GIRAI, cap, gob, ddhh) %>%
+  head(10)
+
+# Medición del p70 en los países
+
+# --------------------------------------------------------------------
 
 # Otras funciones para obtener medidas
 
 # Posición: tendencia central
-mean(datos_limpios$altura) # Media aritmética
-median(datos_limpios$altura) # Mediana
-
-attach(datos_limpios) # Puedo fijar los datos por comodidad
+# mean(datos_limpios$altura) # Media aritmética
+# median(datos_limpios$altura) # Mediana
 
 # Posición: otras
-min(altura) 
-max(altura)
-quantile(altura) # 5 medidas resumen
-quantile(altura, 0.9) # Otros percentiles
-sort(table(especie), decreasing = TRUE)[1] # Moda
+# min(altura) 
+# max(altura)
+# quantile(altura) # 5 medidas resumen
+# quantile(altura, 0.9) # Otros percentiles
+# sort(table(especie), decreasing = TRUE)[1] # Moda
 
 # Dispersión
-range(altura) # Valores mín y max
-max(altura) - min(altura) # Rango
-sd(altura) # Desvío estándar
-var(altura) # Variancia
-IQR(altura) # Rango intercuartílico
-round(sd(altura)/mean(altura)*100,1) # Coeficiente de variación
+# range(altura) # Valores mín y max
+# max(altura) - min(altura) # Rango
+# sd(altura) # Desvío estándar
+# var(altura) # Variancia
+# IQR(altura) # Rango intercuartílico
+# round(sd(altura)/mean(altura)*100,1) # Coeficiente de variación
 
 # Otras medidas
-var(altura,diametro) # Covariancia
-cor(altura,diametro) # Correlación lineal
+# var(altura,diametro) # Covariancia
+# cor(altura,diametro) # Correlación lineal
 
 # Medidas por grupos
-datos_limpios %>% group_by(especie) %>%
-  summarise(Promedio = median(altura),
-            Desv.Est. = IQR(altura),
-            Mínimo = min(altura),
-            Máximo = max(altura))
+# datos_limpios %>% group_by(especie) %>%
+#  summarise(Promedio = median(altura),
+#            Desv.Est. = IQR(altura),
+#            Mínimo = min(altura),
+#           Máximo = max(altura))
 
 # Distribuciones condicionales
-tabyl(datos_limpios, tiempo, follaje) %>%
-  adorn_totals(where = c("row", "col")) %>%
-  adorn_percentages(denominator = "row") %>%
-  adorn_pct_formatting(digits = 1) %>%
-  adorn_title(placement = "top", "Origen", "Tipo de follaje")
+# tabyl(datos_limpios, tiempo, follaje) %>%
+#  adorn_totals(where = c("row", "col")) %>%
+#  adorn_percentages(denominator = "row") %>%
+#  adorn_pct_formatting(digits = 1) %>%
+#  adorn_title(placement = "top", "Origen", "Tipo de follaje")
